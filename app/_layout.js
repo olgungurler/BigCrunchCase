30987eca4ac06353414162b45a12a03fc1abb4f4
Olgun GÜRLER <62409329+olgungurler@users.noreply.github.com>
2025-02-06 01:15:07 +0300

    ProjectFiles
    Project files and router

---
+++ app/_layout.js
@@ -0,0 +1,39 @@
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+    const [loaded] = useFonts({
+        Rubik: require('../assets/fonts/Rubik.ttf'),
+        Epilogue: require('../assets/fonts/Epilogue.ttf'),
+    });
+
+    useEffect(() => {
+        if (loaded) {
+          SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+    
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                    <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+                </Stack>
+                <StatusBar style="auto" />
+            </SafeAreaView>
+            
+        </SafeAreaProvider>
+        
+    );
+}
