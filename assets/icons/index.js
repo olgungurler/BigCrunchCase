d19d22fda2b2a1a1ad0197fbc598256dcb402719
Olgun GÜRLER <62409329+olgungurler@users.noreply.github.com>
2025-02-06 14:05:43 +0300

    Bottom Tab Bar
    Battom tab bar design

---
+++ assets/icons/index.js
@@ -0,0 +1,4 @@
+export { default as HomeIcon } from "./homeIcon";
+export { default as SessionsIcon } from "./sessionsIcon";
+export { default as MessagesIcon } from "./messagesIcon";
+export { default as CommunityIcon } from "./communityIcon";
