f704b3967ff114a534c6c617c25c46d273e7cf0a
Olgun GÜRLER <62409329+olgungurler@users.noreply.github.com>
2025-02-06 14:10:22 +0300

    Prettier

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,33 +1,33 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
-    return(
-        <View style={styles.container}>
-            <View style={styles.welcomeMesageTitle}>
-                <Text style={styles.welcomeMessageText}>Good Afternoon,</Text>
-                <Text style={[styles.welcomeMessageText, styles.bold]}>Sarina!</Text>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.welcomeMesageTitle}>
+        <Text style={styles.welcomeMessageText}>Good Afternoon,</Text>
+        <Text style={[styles.welcomeMessageText, styles.bold]}>Sarina!</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-      backgroundColor: '#FBFBFB'
-    },
-    bold: {
-        fontWeight: 'bold'
-    },
-    welcomeMesageTitle: {
-        paddingHorizontal: 5,
-        paddingVertical: 20
-    },
-    welcomeMessageText: {
-        fontSize: 26,
-        fontWeight: 'regular',
-        fontFamily: 'Epelogue',
-        lineHeight: 32,
-    }
-  });
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#FBFBFB",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  welcomeMesageTitle: {
+    paddingHorizontal: 5,
+    paddingVertical: 20,
+  },
+  welcomeMessageText: {
+    fontSize: 26,
+    fontWeight: "regular",
+    fontFamily: "Epelogue",
+    lineHeight: 32,
+  },
+});
--- app/(tabs)/messages.js
+++ app/(tabs)/messages.js
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
 export default function MessagesScreen() {
-    return(
-        <View style={{ flex: 1, backgroundColor: 'blue' }}>
-            <Text>BU BİR Messages Sayfası</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, backgroundColor: "blue" }}>
+      <Text>BU BİR Messages Sayfası</Text>
+    </View>
+  );
 }
